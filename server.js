f2f9b7e4018e9c8ac3c9da558c62c04a9597065c
varinder19 <vkaur63@gmail.com>
2025-11-07 14:45:31 -0500

    Fix: set views path for Vercel

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
 // Home (landing)
 app.get("/", (req, res) => res.render("home"));
